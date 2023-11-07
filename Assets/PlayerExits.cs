0bc96b6e3b34ca8c90ebcb704291a52f74cfa919
Hunter Pope <hntrpope@gmail.com>
2023-11-06 20:20:18 -0600

    polished the alarm system
    coulding get the on exit function to work

--- Assets/PlayerExits.cs
+++ Assets/PlayerExits.cs
@@ -6,11 +6,20 @@ public class PlayerExits : MonoBehaviour
 {
     private Timer timer_script;
     private GameObject timerObject;
+    private GameObject timerCue;
+    private TimerCue timerCue_script;
+    private GameObject alarmLight;
+    private AudioSource alarmAudio;
     // Start is called before the first frame update
     void Start()
     {
         timerObject = GameObject.FindWithTag("TV");
+        timerCue = GameObject.FindWithTag("TimerCue");
+        alarmLight = GameObject.FindWithTag("AlarmParent");
         timer_script = timerObject.GetComponent<Timer>();
+        timerCue_script = timerCue.GetComponent<TimerCue>();
+        alarmAudio = alarmLight.GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -24,7 +33,12 @@ public class PlayerExits : MonoBehaviour
         if (collision.gameObject.CompareTag("PowerObject"))
         {
             timer_script.enabled = false;
-            this.enabled = false;
+            timerCue_script.enabled = false;
+            //alarmLight.SetActive(false);
+            /*alarmAudio.Pause();
+            alarmLight.GetComponent<Light>().enabled = false;
+            alarmLight.GetComponent<Animator>().enabled = false;*/
+            //this.SetActive(false);
         }
     }
 }
